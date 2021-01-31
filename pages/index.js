def8ea2c976f5246a53cbb451bd71d1686b241d5
Sergio Silva <oldschoolgamer1982@hotmail.com>
2021-01-31 11:40:22 -0300

    Deploy app

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,77 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import React, { useState } from 'react';
+import Head from 'next/head';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget/index';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
-  return <Title>My page</Title>
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Head>
+          <title>Quiz</title>
+        </Head>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
+          <Widget.Header>
+            <h1>Quiz sobre Cinema</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={(infosDoEvento) => {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Digite seu nome"
+                value={name}
+              />
+              <p />
+              <Button type="submit" disabled={name.length === 0}>
+                Começar
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
+          <Widget.Content>
+            <h1>Selecione outro Quiz:</h1>
+            <p> Vazio </p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/oldschoolgamer1982" />
+    </QuizBackground>
+  );
 }
